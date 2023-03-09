7e559b99ac13605060f3e34f20262258d7227406
Adrian Larenaudie <alarenaudie@zaion.ai>
2023-03-09 17:48:48 +0100

    set up message info component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="application_body" id="application_body">
+        <MessageInfo/>
         <RouterView />
     </div>
 </template>
 
-<script setup>
-    import { RouterView } from 'vue-router';
+<script>
+import { RouterView } from 'vue-router';
+import MessageInfo from '@/components/MessageInfo.vue';
+
+export default {
+    name: 'AdminLayoutView',
+    components: {
+        MessageInfo,
+    },
+};
 </script>
 
---
+++ src/components/MessageInfo.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="message_info_block">
+    <p class="message_info_block_content">Voici un message informatif</p>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.message_info_block {
+    z-index: 10000;
+    position: absolute;
+    background-color: rgb(134, 198, 134);
+    border: solid 1px rgb(31, 116, 31);
+    border-radius: .2rem;
+    width: 100%;
+    padding: .6rem 1rem;
+    top: 0;
+    text-align: center;
+}
+.message_info_block_content {
+    font-weight: 600;
+}
+</style>
